7d3028b3bdeec509b7727d9eb9716dcbeff61b6c
Gordon Lee <gordonlee@Gordonui-MacBookAir.local>
2024-04-17 16:34:00 +0900

    Feat: Done Component 제작

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { clearScreenDown } from 'readline'
 import Form from './components/Form'
 import Working from './components/Working'
 
@@ -32,45 +31,3 @@ const App = () => {
 }
 
 export default App
-
-
-// import React, { useState } from 'react';
-// import Form from './components/Form';
-// import { Done, Working } from './components/Working';
-// import "./tailwind.css"
-// // 테일윈드 웨않되?
-
-// type Todo = {
-//   id: number;
-//   title: string;
-//   contents: string;
-//   isDone: boolean;
-// }
-
-// const App: React.FC = () => {
-//   const INITIAL_STATE: Todo[] = [
-//     {
-//       id: 1,
-//       title: 'title1',
-//       contents: 'contents1',
-//       isDone: false,
-//     },
-//   ];
-
-//   const [todos, setTodos] = useState<Todo[]>(INITIAL_STATE);
-
-//   return (
-//     <div className="bg-slate-500 p-40">
-//         {/* <h1 className="text-3xl font-bold underline text-center"> */}
-//         <h1 className="text-xl font-bold underline text-center">
-//           tailwind test
-//         </h1>
-//       <h2>To Do List</h2>
-//       <Form todos={todos} setTodos={setTodos}/>
-//       <Working todos={todos} setTodos={setTodos}/>
-//       <Done todos={todos} setTodos={setTodos}/>
-//     </div>
-//   );
-// };
-
-// export default App;
--- my-app/src/components/Working.tsx
+++ my-app/src/components/Working.tsx
@@ -26,6 +26,7 @@ const Working: React.FC<AppProps> = ({todos, setTodos}) => {
   }
   
   return (
+    <React.Fragment>
     <div>
       <h2>Working</h2>
       {todos.map(todo=> {
@@ -33,89 +34,28 @@ const Working: React.FC<AppProps> = ({todos, setTodos}) => {
           return (
         <div key={todo.id}>
           {todo.title} & {todo.contents}
-          <button onClick={()=>chgBtn(todo)}>cancel</button>  
+          <button onClick={()=>chgBtn(todo)}>{todo.isDone ? "cancel" : "complete"}</button>  
           <button onClick={()=>delBtn(todo.id)}>delete</button>  
         </div>
           )
       })}
-      
-      
     </div>
+        <div>
+        <h2>Done</h2>
+        {todos.map(todo=> {
+          if(todo.isDone)
+            return (
+          <div key={todo.id}>
+            {todo.title} & {todo.contents}
+            <button onClick={()=>chgBtn(todo)}>{todo.isDone ? "cancel" : "complete"}</button>  
+            <button onClick={()=>delBtn(todo.id)}>delete</button>  
+          </div>
+            )
+        })}
+      </div>
+      </React.Fragment>
+
   )
 }
 
 export default Working
-
-// import React from 'react';
-
-// interface Todo {
-//   id: number;
-//   title: string;
-//   contents: string;
-//   isDone: boolean;
-// }
-
-// interface Props {
-//   todos: Todo[];
-//   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-// }
-
-// const Working: React.FC<Props> = ({ todos, setTodos }) => {
-//   const delBtn = (id: number) => {
-//     const rfTodos = todos.filter(el => el.id !== id);
-//     setTodos(rfTodos);
-//   };
-  
-//   const chgBtn = (todo: Todo) => {
-//     todo.isDone = !todo.isDone;
-//     const updatedTodos = todos.map(el => (el.id === todo.id ? todo : el));
-//     setTodos(updatedTodos);
-//   };
-
-//   return (
-//     <div>
-//       <h2>WORKING</h2>
-//       {todos.map(todo => {
-//         if (!todo.isDone)
-//           return (
-//             <div key={todo.id}>
-//               {todo.title} : {todo.contents}
-//               <button onClick={() => delBtn(todo.id)}>del</button>
-//               <button onClick={() => chgBtn(todo)}>{todo.isDone ? 'canc' : 'done'}</button>
-//             </div>
-//           );
-//       })}
-//     </div>
-//   );
-// };
-
-// const Done: React.FC<Props> = ({ todos, setTodos }) => {
-//   const delBtn = (id: number) => {
-//     const rfTodos = todos.filter(el => el.id !== id);
-//     setTodos(rfTodos);
-//   };
-  
-//   const chgBtn = (todo: Todo) => {
-//     todo.isDone = !todo.isDone;
-//     const updatedTodos = todos.map(el => (el.id === todo.id ? todo : el));
-//     setTodos(updatedTodos);
-//   };
-
-//   return (
-//     <div>
-//       <h2>DONE</h2>
-//       {todos.map(todo => {
-//         if (todo.isDone)
-//           return (
-//             <div key={todo.id}>
-//               {todo.title} : {todo.contents}
-//               <button onClick={() => delBtn(todo.id)}>del</button>
-//               <button onClick={() => chgBtn(todo)}>{todo.isDone ? 'canc' : 'done'}</button>
-//             </div>
-//           );
-//       })}
-//     </div>
-//   );
-// };
-
-// export { Working, Done };
