be1ea7c2caf3d9f660f1ef5e6015938a96553270
Gordon Lee <gordonlee@Gordonui-MacBookAir.local>
2024-04-17 15:37:26 +0900

    Fix: Plug-in: tailwind set-up commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Form from './components/Form';
 import { Done, Working } from './components/Working';
+import "./tailwind.css"
+// 테일윈드 웨않되?
 
-
-interface Todo {
+type Todo = {
   id: number;
   title: string;
   contents: string;
@@ -23,7 +24,11 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>(INITIAL_STATE);
 
   return (
-    <div>
+    <div className="bg-slate-500 p-40">
+        {/* <h1 className="text-3xl font-bold underline text-center"> */}
+        <h1 className="text-xl font-bold underline text-center">
+          tailwind test
+        </h1>
       <h2>To Do List</h2>
       <Form todos={todos} setTodos={setTodos}/>
       <Working todos={todos} setTodos={setTodos}/>
